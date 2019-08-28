4cc193959625aeb7b9d0bd4c03dd261cd12cb9ca
ariel415el <ariel415el@gmail.com>
2019-08-28 10:30:03 +0300

    add gppu specification to config

--- main_project/config.py
+++ main_project/config.py
@@ -6,22 +6,26 @@ parser = argparse.ArgumentParser(description='PyTorch time series Anomaly detect
 parser.add_argument('--debug', action='store_true',default=False)
 parser.add_argument('--test', action='store_true', default=False)
 parser.add_argument('--data_type', type=str, default="2d")
-parser.add_argument('--trained_model', type=str, default=R"W:\AnomalyDetection\TimeSeriesAnomalyUsingTransformations\random_matrices\train_dir_2d\BB_vid_0.79570_ckp.pt")
-parser.add_argument('--train_dir', type=str, default="train_dir_2d")
+parser.add_argument('--trained_model', type=str, default=R"")
+parser.add_argument('--train_dir', type=str, default="")
 
 # common parameters
-parser.add_argument('--num_train_series', type=int, default=1000)
+parser.add_argument('--num_train_series', type=int, default=5000)
 parser.add_argument('--num_val_series', type=int, default=100)
 parser.add_argument('--num_transforms', type=int, default=100)
-parser.add_argument('--transformations_type', type=str, default="Permutations", help='Permutations/Affine')
-
+parser.add_argument('--transformations_type', type=str, default="Affine", help='Permutations/Affine')
 # 1d dataset config
-parser.add_argument('--func_type', type=str, default="Linear", help='Linear/Sinus-Linear/Power-Sinus')
+parser.add_argument('--func_type', type=str, default="Power-Sinus", help='Linear/Sinus-Linear/Power-Sinus')
 
 # 2d dataset config
 parser.add_argument('--anomaly_type', type=str, default="Shapes", help='Shapes/Incontinous')
 parser.add_argument('--frame_h', type=int, default=128)
 parser.add_argument('--frame_w', type=int, default=128)
+
+# Pytorch parameters
+parser.add_argument('--num_workers', type=int, default=0)
+parser.add_argument('--gpu_id', type=int, default=0)
+
 
 args = parser.parse_args()
 
@@ -29,20 +33,21 @@ if args.data_type == "2d":
     args.num_frames = 64
     args.segment_size = 1
     args.batch_size = 80
+    if args.train_dir == "":
+        args.train_dir = "training-2d-%s-%s"%(args.transformations_type, args.anomaly_type)
 else:
     args.batch_size = 256
     args.num_frames = 128
     args.segment_size = 16
+    if args.train_dir == "":
+        args.train_dir = "training-1d-%s-%s"%(args.transformations_type, args.func_type)
 
 # Pytorch parameters
-args.num_workers = 0
-args.pin_memory = True
+args.pin_memory = False
 args.seed = 1
 
 # train hyper parameters
-args.epochs = 10
+args.epochs = 40
 args.lr = 0.0001
 args.lr_decay = 0.5
-args.decay_epochs = 1
-args.num_workers =0
-args.num_workers =0
+args.decay_epochs = 10
